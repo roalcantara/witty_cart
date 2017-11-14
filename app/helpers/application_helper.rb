be61dec15a6ae67a51f829110b61d33f3c860f74
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2017-11-14 04:58:46 -0200

    ref(registrations): Display error messages
    Before that, the errors messages were been displayed only via toast.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,14 @@ module ApplicationHelper
   def current_year
     Date.current.year
   end
+
+  def error?(resource, field)
+    'is-invalid' if resource.errors[field].any?
+  end
+
+  def error(resource, field)
+    content_tag :div,
+                resource.errors[field].join(', ').html_safe,
+                class: 'invalid-feedback d-block mt-0' if resource.errors[field].any?
+  end
 end
