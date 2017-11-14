fad72632b096a23f42019115befc599767a6bb14
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2017-11-14 20:34:51 -0200

    ref(application-records): Add pluralized_model_name method
    Which pluralises and returns a model name.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.pluralized_model_name
+    model_name.human count: 2
+  end
 end
--- spec/models/application_record_spec.rb
+++ spec/models/application_record_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ApplicationRecord do
   it { expect(described_class.ancestors).to include ActiveRecord::Base }
+
+  describe '.pluralized_model_name' do
+    it 'pluralizes its model name' do
+      expect(User.pluralized_model_name).to eq 'Users'
+    end
+  end
 end
