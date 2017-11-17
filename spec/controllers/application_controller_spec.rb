cd61870d922712a645065ad0b0fdbf2ad06f965b
Rogério Rodrigues de Alcântara <rogerio.alcantara@gmail.com>
2017-11-17 18:17:11 -0200

    ref(UX): Add breadcrumbs to Users
    Following the theme UX pattern.

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ApplicationController do
   it_behaves_like 'breadcrumbable'
+
+  context 'when it is a devise controller' do
+    controller(ApplicationController) do
+      def index
+        head :ok
+      end
+    end
+
+    before { expect(controller).to receive(:devise_controller?).and_return true }
+
+    it 'renders a breadcrumb' do
+      get 'index'
+
+      expect(assigns(:breadcrumbs)).to include 'User'
+    end
+  end
 end
